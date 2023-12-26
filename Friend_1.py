b7490a134193be804f6fe4b7aeea586812c19347
mertcank1 <52007872+mertcank1@users.noreply.github.com>
2023-12-26 15:06:25 +0300

    Add files via upload

---
+++ Friend_1.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+@author: CS115
+"""
+
+class Friend( object ):
+    def __init__( self, name, phone, year ):
+    	self.fname = name
+    	self.fphone = phone
+    	self.year = year
+
+   
+f1 = Friend('Jane Doe','555-1234',2015)
+f2 = Friend('John Smith','555-9876',2017)
